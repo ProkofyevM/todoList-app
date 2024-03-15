d17659d68253c5247c8ecc1a1ee8798e81d05272
Миша Прокофьев <misaprokofev@MacBook-Air--Misha.local>
2024-03-15 14:54:29 +0300

    add task1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	const [todoList, setTodolist] = useState([])
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((response) => response.json())
+			.then((loadedList) => {
+				setTodolist(loadedList)
+			})
+	}, [])
+
+	const onChange = ({ target }) => {
+		console.log(target.value)
+	}
+
+	return (
+		<div className="app">
+			<form className="form">
+				<input
+					className="input"
+					placeholder="Введите задачу...."
+					type="text"
+					onChange={onChange}
+				></input>
+				<button className="btn1">Добавить</button>
+			</form>
+			{todoList.map(({ id, title }) =>
+				id <= 7 ? (
+					<div key={id} className="item">
+						Задача: {title}
+					</div>
+				) : null,
+			)}
+		</div>
+	)
 }
-
-export default App;
